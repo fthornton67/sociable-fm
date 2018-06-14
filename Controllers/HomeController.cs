3559dc8d1175fd4b049d9dc48522366bf8e48550
fthornton67 <fthornton67@gmail.com>
2018-06-14 16:07:14 -0700

    update project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace core_app.Controllers
+namespace sociable_fm.Controllers
 {
     public class HomeController : Controller
     {
--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace core_app.Controllers
+namespace sociable_fm.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
