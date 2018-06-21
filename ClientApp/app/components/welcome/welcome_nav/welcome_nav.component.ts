f328946ff309e17cf6f7ec796475224d915673b0
fthornton67 <fthornton67@gmail.com>
2018-06-21 16:49:49 -0700

    clean

--- ClientApp/app/components/welcome/welcome_nav/welcome_nav.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'welcome-nav',
-    templateUrl: './welcome_nav.component.html',
-    styleUrls: ['./welcome_nav.component.css']
-})
-export class WelcomeNavComponent {
-}
--- ClientApp/app/components/welcome/welcome_vid/welcome_vid.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'welcome-vid',
-    templateUrl: './welcome_vid.component.html',
-    styleUrls: ['./welcome_vid.component.css']
-})
-export class WelcomeVidComponent {
-}
